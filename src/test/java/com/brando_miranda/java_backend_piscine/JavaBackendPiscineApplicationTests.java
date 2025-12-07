253e77f5c4cdfd3bfa0f45f395604e4b3e3c8a82
Hildebrando Miranda <mirandahildebrando5@gmail.com>
2025-12-07 11:21:15 -0300

    Project completed, working on tests

--- src/test/java/com/brando_miranda/java_backend_piscine/JavaBackendPiscineApplicationTests.java
+++ src/test/java/com/brando_miranda/java_backend_piscine/JavaBackendPiscineApplicationTests.java
@@ -3,7 +3,7 @@ package com.brando_miranda.java_backend_piscine;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+
 
 
 class JavaBackendPiscineApplicationTests {
